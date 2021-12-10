b308f2896e405a34f04fc7acb7314677e53d33fe
ALex <Leha.lapshin1996@mail.ru>
2021-12-10 20:41:30 +0300

    Dz -2

---
+++ Algoritmi/DZ-2/Prime-not prime/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n)
+{
+  int i;
+  for(i=2;i<n;i++)
+  {
+      if(n%i == 0 && i!=n)
+        return 0;
+  }
+   return 1;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+
+    scanf_s("%d",&n);
+
+    printf("A number is :" "%d\n", isPrime(n));
+
+    // 1 - простое
+    // 0 - не простое
+
+    return 0;
+}
