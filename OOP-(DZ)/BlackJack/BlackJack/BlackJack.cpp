d6a840ca17c8d73eff2796e642094b25a9a44616
ALex <Leha.lapshin1996@mail.ru>
2022-02-28 17:11:42 +0300

    Blackjack(ver.1.01)

--- OOP-(DZ)/BlackJack/BlackJack/BlackJack.cpp
+++ OOP-(DZ)/BlackJack/BlackJack/BlackJack.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include<string>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -201,10 +202,11 @@ public:
 	{
 		cout << p_Name << " - you have a draw" << endl;
 	}
-	bool IsHitting() override
+
+	bool IsHitting() override 
 	{
 		char answ;
-		cout << p_Name << " do you need another card? (Y or N) :" << endl;
+		cout <<p_Name<<" do you need another card? (Y or N) :" << endl;
 		cin >> answ;
 		if (answ == 'Y' || answ == 'y')
 		{
@@ -223,7 +225,11 @@ public:
 class House : public GenericPlayer
 {
 public:
-	House(const string&name = "Dealer"){}
+	House(const string &name ) : GenericPlayer(name) {}
+	House()
+	{
+		p_Name = "Dealer";
+	}
 	bool IsHitting() override
 	{
 		if(GetTotal() <=16)
@@ -253,15 +259,200 @@ private:
 
 };
 
+class Deck : public Hand
+{
+public:
+	Deck()
+	{
+		vec_cards.reserve(52);
+		Populate();
+	}
+	void Populate()
+	{
+		Clear();
+		for (int suit = Card::Diamonds; suit <= Card::Spades; suit++)
+		{
+			for (int cardValue = Card::Ace; cardValue <= Card::King; cardValue++)
+			{
+				Add(new Card(static_cast<Card::CardMeaning>(cardValue),static_cast<Card::Suit>(suit)));
+			}
+		}
+	}
 
+	void Shuffle()
+	{
+		random_shuffle(vec_cards.begin(), vec_cards.end());
+	}
 
+	void Deal(Hand &hand_h)
+	{
+		if (!vec_cards.empty())
+		{
+			hand_h.Add(vec_cards.back());
+			vec_cards.pop_back();
+		}
+		else
+		{
+			cout << "The deck is out of cards" << endl;
+		}
+	}
+
+	void AdditionalCards(GenericPlayer &generic_player)
+	{
+		while (!(generic_player.IsBoosted()) && generic_player.IsHitting())
+		{
+			Deal(generic_player);
+			cout << generic_player << endl;
+			if (generic_player.IsBoosted())
+			{
+				generic_player.Bust();
+			}
+		}
+	}
+
+	virtual ~Deck(){}
+};
+
+class Game
+{
+public:
+	Game(const vector<string>& names)
+	{
+		vector<string> ::const_iterator itNames;
+		for (itNames  = names.begin();itNames != names.end();itNames ++)
+		{
+			player_game.push_back(Player(*itNames));
+		}
+		srand(static_cast<unsigned int>(time(NULL)));
+		deck_game.Populate();
+		deck_game.Shuffle();
+	}
+
+	void Play()
+	{
+		vector<Player>::iterator itPlayer;
+		for (int i = 0; i < 2; i++)
+		{
+			for (itPlayer  = player_game.begin();itPlayer!=player_game.end();itPlayer++)
+			{
+				deck_game.Deal(*itPlayer);
+			}
+			deck_game.Deal(house_game);
+		}
+		house_game.FlipFirstCard();
+
+		for (itPlayer  = player_game.begin();itPlayer!=player_game.end();itPlayer++)
+		{
+			cout << *itPlayer << endl;
+		}
+		cout << house_game << endl;
+
+		for (itPlayer = player_game.begin();itPlayer!=player_game.end();itPlayer++)
+		{
+			deck_game.AdditionalCards(*itPlayer);
+		}
+		house_game.FlipFirstCard();
+		cout << endl << house_game;
+		deck_game.AdditionalCards(house_game);
+		if (house_game.IsBoosted())
+		{
+			for (itPlayer = player_game.begin();itPlayer!=player_game.end();itPlayer++)
+			{
+				if (!(itPlayer->IsBoosted()))
+				{
+					itPlayer->Win();
+				}
+			}
+		}
+		else
+		{
+			for (itPlayer = player_game.begin();itPlayer != player_game.end();itPlayer++)
+			{
+				if (!(itPlayer->IsBoosted()))
+				{
+					if (itPlayer->GetTotal() > house_game.GetTotal())
+					{
+						itPlayer->Win();
+					}
+					else if (itPlayer->GetTotal() < house_game.GetTotal())
+					{
+						itPlayer->Lose();
+					}
+					else
+					{
+						itPlayer->Push();
+					}
+				}
+			}
+		}
+
+		for (itPlayer = player_game.begin();itPlayer!=player_game.end();itPlayer++)
+		{
+			itPlayer->Clear();
+		}
+		house_game.Clear();
+	}
+	
+	~Game(){}
+
+private:
+	Deck deck_game;
+	House house_game;
+	vector<Player> player_game;
+};
+
+void PlayAgain(Game& newGame)
+{
+	char answer = 'y';
+	while (answer == 'y' || answer == 'Y')
+	{
+		newGame.Play();
+		cout << "Do you want play again?" << endl;
+		cout << "(Y) or (N)" << endl;
+		cin >> answer;
+		cout << "\n";
+		if (answer == 'n' || answer == 'N')
+		{
+			cout << "Thanks you for playing BlackJack,goodbye" << endl;
+		}
+	}
+}
 
 
 int main()
 {
-
 	
-    
+	int NumberofPlayers = 0;
+	while (NumberofPlayers < 1 || NumberofPlayers > 7 )
+	{
+		cout << "How many players will play game,1 - 7 players?" << endl;
+		while (!(cin>>NumberofPlayers))
+		{
+			cin.clear();
+			while (cin.get() !='\n')
+			{
+				
+			}
+			cout << "Error,please enter count of players" << endl;
+			cout << "How many players?" << endl;
+		}
+		
+	}
+	vector<string> names;
+	string name;
+
+	for (int i = 0; i < NumberofPlayers; i++)
+	{
+		cout << "Enter name of player : ";
+		cin >> name;
+		names.push_back(name);
+	}
+	cout << "\n\n";
+
+	Game newGame(names);
+
+	PlayAgain(newGame);
+	
 }
 
 
