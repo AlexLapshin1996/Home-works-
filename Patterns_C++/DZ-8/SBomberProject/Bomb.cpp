12504a6452633a7dd4e33065ff798bbe61dcfef3
ALex <Leha.lapshin1996@mail.ru>
2022-09-24 12:08:14 +0300

    Patterns_C++ DZ-8

---
+++ Patterns_C++/DZ-8/SBomberProject/Bomb.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+#include "Bomb.h"
+#include "MyTools.h"
+
+using namespace std;
+using namespace MyTools;
+
+void Bomb::Draw() const
+{
+    MyTools::SetColor(CC_LightMagenta);
+    GotoXY(x, y);
+    cout << "*";
+}
+
+Bomb* Bomb::ClastBomb() const
+{
+    return new Bomb(*this);
+}
